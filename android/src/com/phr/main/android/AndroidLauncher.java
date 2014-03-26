ca3a525cc3b3b91ef7d6027eca4969156f818889
Brandon <bmaxwell921@gmail.com>
2014-03-26 18:11:12 -0500

    Initial project setup

---
+++ android/src/com/phr/main/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.phr.main.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.phr.main.PixHellGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new PixHellGame(), config);
+	}
+}
---
+++ desktop/src/com/phr/main/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.phr.main.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.phr.main.PixHellGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new PixHellGame(), config);
+	}
+}
