fafb2c4a29f6fdb305a07d9535dec4f6ef40612e
Brandon <bmaxwell921@gmail.com>
2014-03-26 19:57:53 -0500

    Initial implementation of game

--- core/src/com/phr/main/PixHellGame.java
+++ core/src/com/phr/main/PixHellGame.java
@@ -1,27 +1,30 @@
 package com.phr.main;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.phr.main.screens.GameScreen;
 
-public class PixHellGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+public class PixHellGame extends Game {
+
+	public static final int WIDTH = 480;
+	public static final int HEIGHT = 800;
+	
+	// SB used for drawing sprites
+	public SpriteBatch batch;
 	
 	@Override
-	public void create () {
+	public void create() {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		this.setScreen(new GameScreen(this));
 	}
-
+	
 	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+	public void render() {
+		super.render();
+	}
+	
+	@Override
+	public void dispose() {
+		batch.dispose();
 	}
 }
