72e7a15cfc25b827cc3dc19911f33b0528c50efa
Brandon <bmaxwell921@gmail.com>
2014-03-26 19:56:19 -0500

    Added texture packing

--- desktop/src/com/phr/main/desktop/DesktopLauncher.java
+++ desktop/src/com/phr/main/desktop/DesktopLauncher.java
@@ -2,11 +2,20 @@ package com.phr.main.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.tools.imagepacker.TexturePacker2;
 import com.phr.main.PixHellGame;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		packImages();
+		config.title = "PixHellRedux";
+		config.width = PixHellGame.WIDTH;
+		config.height = PixHellGame.HEIGHT;
 		new LwjglApplication(new PixHellGame(), config);
+	}
+	
+	private static void packImages() {
+		TexturePacker2.process("../images", "../android/assets", "gameImages");
 	}
 }
