3a01a6d2d34d572181959a466b27b0f0d5219bca
Brandon <bmaxwell921@gmail.com>
2014-03-30 15:45:43 -0500

    System to keep playing in the screen

---
+++ core/src/com/phr/main/systems/PlayerScreenCollisionSys.java
@@ -0,0 +1,40 @@
+package com.phr.main.systems;
+
+import com.artemis.ComponentMapper;
+import com.artemis.Entity;
+import com.artemis.Filter;
+import com.artemis.systems.EntityProcessingSystem;
+import com.phr.main.PixHellGame;
+import com.phr.main.components.DimensionComp;
+import com.phr.main.components.PlayerComp;
+import com.phr.main.components.PositionComp;
+
+public class PlayerScreenCollisionSys extends EntityProcessingSystem {
+
+	private ComponentMapper<PositionComp> pcm;
+	private ComponentMapper<DimensionComp> dcm;
+	
+	public PlayerScreenCollisionSys() {
+		super(Filter.allComponents(PositionComp.class, DimensionComp.class, PlayerComp.class));
+	}
+	
+	@Override
+	public void initialize() {
+		pcm = world.getMapper(PositionComp.class);
+		dcm = world.getMapper(DimensionComp.class);
+	}
+	
+	@Override
+	public void process(Entity e) {
+		// Check if the player hit the edge of the screen
+		PositionComp pc = pcm.get(e);
+		DimensionComp dc = dcm.get(e);
+		
+		if (pc.position.x <= 0) {
+			pc.position.x = 0;
+		}
+		if (pc.position.x + dc.width > PixHellGame.VIRTUAL_WIDTH) {
+			pc.position.x = PixHellGame.VIRTUAL_WIDTH - dc.width;
+		}
+	}
+}
